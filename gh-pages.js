a1f576698339744ebbfdefd5b13676d1e0a7b571
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2022-06-24 17:42:01 +0200

    added gh pages

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/dalleman1/svelte-app.git', // Update to point to your repository  
+        user: {
+            name: 'Morten Dalsgaard', // update to use your name
+            email: 'monglade.dalsgaard@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
